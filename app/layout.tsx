7acba31150e938b367d10c643456b09b3990b5e3
unknown <talukdargour.bd@gmail.com>
2025-07-19 18:20:25 +0600

    set primary and secondary theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
         <QueryClientProvider client={queryClient}>
           <ThemeProvider 
             attribute="class" 
-            defaultTheme="system" 
-            enableSystem 
+            defaultTheme="light" 
+            enableSystem={false}
             disableTransitionOnChange
           >
             {children}
